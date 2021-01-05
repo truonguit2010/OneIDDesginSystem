c364a5103bfb00d9a64003642782605217f8121f
Pham Son Truong <truongps@sendo.vn>
2021-01-05 16:02:30 +0700

    - Finish define layout.

---
+++ one_id_design_system/one_id_design_system/lib/one_id_layout.dart
@@ -0,0 +1,35 @@
+
+///
+/// https://ds.id.vin/691ac4900/v/5405/p/58ad3f-layout
+///
+class ODSSpacing {
+  static const double XXS = 4;
+  static const double XS = 8;
+  static const double S = 12;
+  static const double M = 16;
+  static const double L = 24;
+  static const double XL = 32;
+  static const double XXL = 48;
+}
+
+///
+/// https://ds.id.vin/691ac4900/v/5405/p/58ad3f-layout
+///
+class ODSFullColumnLayout {
+  static const double PADDING = 16;
+
+  static const double SPACING = 12;
+}
+
+class ODSCarouselLayout {
+  static const double PADDING_LEFT = 16;
+  static const double PADDING_RIGHT = 0;
+  static const double SPACING = 12;
+}
+
+class ODSIconSize {
+  static const double SIZE48 = 48;
+  static const double SIZE40 = 40;
+  static const double SIZE24 = 24;
+  static const double SIZE16 = 16;
+}
